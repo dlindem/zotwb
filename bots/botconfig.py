a02f1b4a4bc5fd98996c1b76e08737baa4534f3b
David Lindemann <34068491+dlindem@users.noreply.github.com>
2023-11-20 17:54:01 +0100

    Handle config_private does not exist

--- bots/botconfig.py
+++ bots/botconfig.py
@@ -5,6 +5,11 @@ import os
 with open("profiles.json", 'r', encoding='utf-8') as file:
     profile = json.load(file)['last_profile']
 
+# check if config_private exists for this profile
+if not os.path.exists(f"profiles/{profile}/config_private.json"):
+    with open(f"profiles/{profile}/config_private.json", 'w', encoding='utf-8') as file:
+        json.dump({"wb_bot_user": None, "wb_bot_pwd": None, "zotero_api_key": None}, file)
+
 def load_mapping(mappingname):
     # print(f"Will load mapping: {mappingname}.json")
     with open(f"profiles/{profile}/{mappingname}.json", 'r', encoding='utf-8') as jsonfile:
